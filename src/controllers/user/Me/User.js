85125d50768fff4076f289ce3525c3dc25dc3a0f
Murilo <murigod8@gmail.com>
2025-10-28 21:08:38 -0300

    fixed: fixed user get (again)

--- src/controllers/user/Me/User.js
+++ src/controllers/user/Me/User.js
@@ -14,7 +14,7 @@ router.get("/", async (req, res) => {
     const user = await prisma.user.findUnique({
       where: { id: userId },
       include: {
-        fops: { select: { createdAt: true, updateAt: true, status: true } },
+        fops: { select: { createdAt: true, updatedAt: true, status: true } },
         department: {
           select: { id: true, name: true },
         },
